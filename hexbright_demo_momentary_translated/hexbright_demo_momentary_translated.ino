c49afe52a85a04733951c13d4306a87ce6751475
David P Hilton <david.hilton.p@gmail.com>
2013-01-03 09:54:42 -0700

    translated demo_momentary to the hexbright library

---
+++ hexbright_demo_momentary_translated/hexbright_demo_momentary_translated.ino
@@ -0,0 +1,27 @@
+/*
+  Hexbright demo firmware: Momentary
+  Andrew Magill  9/2012
+  
+  Light turns on when the button is down.  That's it.
+*/
+
+#include <hexbright.h>
+#include <Wire.h>
+hexbright hb;
+
+void setup()
+{
+  hb.init_hardware();
+}
+
+void loop()
+{
+  hb.update();
+  if (hb.button_held()) // technically, returns a length of time, but any non-zero length of time is true
+    hb.set_light(MAX_LEVEL, MAX_LEVEL, NOW);
+  else
+  // We could set_light(CURRENT_LEVEL, 0, NOW), but this will
+  // actually turn us off if we're not plugged in.
+    hb.shutdown();
+}
+
