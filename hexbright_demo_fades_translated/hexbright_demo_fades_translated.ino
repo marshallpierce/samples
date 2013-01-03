3ff91bd857bf769034b6eb280d1650e50994402e
David P Hilton <david.hilton.p@gmail.com>
2013-01-03 10:26:22 -0700

    mostly translated demo_fades to hexbright library, some polish remains

--- hexbright_demo_fades_translated/hexbright_demo_fades_translated.ino
+++ hexbright_demo_fades_translated/hexbright_demo_fades_translated.ino
@@ -7,90 +7,61 @@
   3. Press button.  Light turns off.
 */
 
-// Pin assignments
-#define DPIN_RLED_SW            2
-#define DPIN_GLED               5
-#define DPIN_PWR                8
-#define DPIN_DRV_MODE           9
-#define DPIN_DRV_EN             10
+#include <hexbright.h>
+#include <Wire.h>
+
+hexbright hb;
+
 // Modes
 #define MODE_OFF                0
 #define MODE_FADE               1
 #define MODE_ON                 2
 
 byte mode;
-boolean btnDown;
-unsigned long lastTime, btnTime;
-int bright, fadeDir;
 
 void setup()
 {
-  pinMode(DPIN_PWR,      INPUT);
-  digitalWrite(DPIN_PWR, LOW);
-
-  // Initialize GPIO
-  pinMode(DPIN_RLED_SW,  INPUT);
-  pinMode(DPIN_GLED,     OUTPUT);
-  pinMode(DPIN_DRV_MODE, OUTPUT);
-  pinMode(DPIN_DRV_EN,   OUTPUT);
-  digitalWrite(DPIN_DRV_MODE, HIGH);
-  digitalWrite(DPIN_DRV_EN,   LOW);
-  digitalWrite(DPIN_GLED, HIGH);
+  hb.init_hardware();
   
   mode = MODE_OFF;
-  btnDown = digitalRead(DPIN_RLED_SW);
-  btnTime = millis();
 }
 
 void loop()
 {
-  unsigned long time = millis();
-  boolean newBtnDown = digitalRead(DPIN_RLED_SW);
-  
+  hb.update();
   switch (mode)
   {
   case MODE_OFF:
-    if (btnDown && newBtnDown && time-btnTime>50)
-    {
+    if(hb.button_held()) {
       mode = MODE_FADE;
-      bright = 0;
-      fadeDir = 1;
-      pinMode(DPIN_PWR,      OUTPUT);
-      digitalWrite(DPIN_PWR, HIGH);
     }
     break;
   case MODE_FADE:
-    if (time-lastTime > 5)
+    if(hb.button_held())
     {
-      lastTime = time;
-      if (fadeDir>0 && bright==255) fadeDir = -1;
-      if (fadeDir<0 && bright==0  ) fadeDir =  1;
-      bright += fadeDir;
-      unsigned int gamma = (bright*bright)>>8;
-      if (gamma < 6) gamma = 6;
-      analogWrite(DPIN_DRV_EN, gamma);
+      if(!hb.light_change_remaining())
+        if(hb.get_light_level()<500) // the light is low, go up from here
+          hb.set_light(CURRENT_LEVEL, 1000, 1000);  // go from low to high over 2 seconds
+        else
+          hb.set_light(CURRENT_LEVEL, 1, 1000); // go from high to low over 2 seconds
     }
-    if (!btnDown && !newBtnDown && time-btnTime>50)
+    else // released!
+    { // use the current light level for our brightness
+      hb.set_light(CURRENT_LEVEL, CURRENT_LEVEL, NOW);
       mode = MODE_ON;
+    }
     break;
   case MODE_ON:
-    if (btnDown && newBtnDown && time-btnTime>200)
-      mode = MODE_FADE;
-    if (btnDown && !newBtnDown)
+    if (hb.button_held()<200 && hb.button_released())
     {
+      hb.shutdown();
       mode = MODE_OFF;
-      digitalWrite(DPIN_DRV_EN, LOW);
-      pinMode(DPIN_PWR,      OUTPUT);
-      digitalWrite(DPIN_PWR, LOW);
+    }
+    else if (hb.button_held()>200) 
+    {
+      mode = MODE_FADE; 
     }
     break;    
-  }
-  
-  if (newBtnDown != btnDown)
-  {
-    btnTime = time;
-    btnDown = newBtnDown;
-    delay(50);
   }
 }
 
